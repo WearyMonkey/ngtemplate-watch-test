4d165b3d76c0df01edc989e950b9ba3e15cc408a
toby <toby@canva.com>
2017-03-11 13:48:00 +0900

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+	entry: './entry',
+	module: {
+		rules: [
+			{
+				test: /\.html$/,
+				use: [
+					{ 
+						loader: 'ngtemplate-loader',
+						options: {
+            				relativeTo: __dirname
+          				}
+					}, 
+					'html-loader'
+				]
+			}
+		]
+	},
+	output: {
+		filename: 'out/bundle.js'
+	}
+};
